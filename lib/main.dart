c2afdc4d36a41ce583902cb4edd648f2f5848a96
Paulo Roberto De Morais <prmorais1302@gmail.com>
2025-12-22 13:11:32 -0300

    Implementing splash page.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_spotify/core/configs/theme/app_theme.dart';
+import 'package:flutter_spotify/presentation/splash/pages/splash_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,9 +13,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Spotify',
       theme: AppTheme.lightTheme,
-      home: Container(),
+      title: 'Flutter Spotify',
+      debugShowCheckedModeBanner: false,
+      home: const SplashPage(),
     );
   }
 }
